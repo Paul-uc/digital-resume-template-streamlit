7b70587af31b0baa57ebd6b75b8c3d8880bd433f
Siew Ong <80304168+Paul-uc@users.noreply.github.com>
2024-01-04 16:32:07 +0800

    information update

--- app.py
+++ app.py
@@ -12,24 +12,22 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Paul Wong"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Paul Wong"
 DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+Junior Developer, assisting enterprises by supporting application-development.
 """
-EMAIL = "johndoe@email.com"
+EMAIL = "siewong007@gmail.com"
 SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
+    "YouTube": "https://youtube.com/",
     "LinkedIn": "https://linkedin.com",
     "GitHub": "https://github.com",
     "Twitter": "https://twitter.com",
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Laboratory Management System - the complete Lab Management System": "https://github.com/Paul-uc/laboratory-stock-management",
+    "🏆 Parking Management System - Android Application for school": "https://github.com/Paul-uc/ParkingManagementSystem",
 }
 
 
@@ -73,8 +71,8 @@ st.write('\n')
 st.subheader("Experience & Qulifications")
 st.write(
     """
-- ✔️ 7 Years expereince extracting actionable insights from data
-- ✔️ Strong hands on experience and knowledge in Python and Excel
+- ✔️ Degree Holder in Software System Developmet
+- ✔️ Strong hands on experience and knowledge in Java and Excel
 - ✔️ Good understanding of statistical principles and their respective applications
 - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
 """
@@ -86,9 +84,9 @@ st.write('\n')
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decition trees
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, Java, PHP
+- 📊 Data Visulization: PowerBi
+- 📚 Modeling: on-the-way
 - 🗄️ Databases: Postgres, MongoDB, MySQL
 """
 )
@@ -100,39 +98,38 @@ st.subheader("Work History")
 st.write("---")
 
 # --- JOB 1
-st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-st.write("02/2020 - Present")
+st.write("🚧", "**Junior Developer | Simplr Sdn Bhd**")
+st.write("02/2023 - Present")
 st.write(
     """
 - ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
 - ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-- ► Redesigned data model through iterations that improved predictions by 12%
 """
 )
 
-# --- JOB 2
-st.write('\n')
-st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-st.write("01/2018 - 02/2022")
-st.write(
-    """
-- ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
-- ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-- ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
-"""
-)
+# # --- JOB 2
+# st.write('\n')
+# st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
+# st.write("01/2018 - 02/2022")
+# st.write(
+#     """
+# - ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
+# - ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
+# - ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
+# """
+# )
 
-# --- JOB 3
-st.write('\n')
-st.write("🚧", "**Data Analyst | Chegg**")
-st.write("04/2015 - 01/2018")
-st.write(
-    """
-- ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
-- ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-- ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
-"""
-)
+# # --- JOB 3
+# st.write('\n')
+# st.write("🚧", "**Data Analyst | Chegg**")
+# st.write("04/2015 - 01/2018")
+# st.write(
+#     """
+# - ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
+# - ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
+# - ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
+# """
+# )
 
 
 # --- Projects & Accomplishments ---
